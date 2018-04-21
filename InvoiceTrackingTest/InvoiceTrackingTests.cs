cc2263b425529ac62aba3bb91920e0f16559f3ea
César Almeida <cesargralmeida@gmail.com>
2018-04-20 22:09:46 -0300

    Testing Commid

--- InvoiceTrackingTest/InvoiceTrackingTests.cs
+++ InvoiceTrackingTest/InvoiceTrackingTests.cs
@@ -9,7 +9,7 @@ namespace InvoiceTrackingTest
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual(true, true);
+            Assert.AreEqual(false, false);
         }
     }
 }
